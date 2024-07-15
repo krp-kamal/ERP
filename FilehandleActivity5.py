03a25174b028b7c8d055ce18e48126f52cafc0ac
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-07-15 17:33:31 +0100

    Update and rename FilehandleActivity5.txt to FilehandleActivity5.py

--- FilehandleActivity5.py
+++ FilehandleActivity5.py
@@ -1,4 +1,8 @@
- # reads the first four characters in the file #
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+# reads the first four characters in the file #
 robj= open('/Users/kamalperumal/Downloads/r3.log','r')
 print(robj.read(5))
 robj.close()
