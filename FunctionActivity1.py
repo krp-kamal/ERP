b2fa07d403558193cc3493f15eed7ce3ab652ddf
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-07-16 21:50:20 +0100

    Create FunctionActivity1.py

---
+++ FunctionActivity1.py
@@ -0,0 +1,19 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+import os
+def f1():
+    print('list of files:')
+    os.system('ls')
+def f2():
+    print('process details:')
+    os.system('ps')
+
+print('this is the main scrip section')
+f1()
+print('')
+f2()
+f1()
+f2()
+print('exit from the scrip section')
