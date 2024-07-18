f09f5d2476ae5fcad6af6c9681f62b3e4eaed260
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-07-18 22:48:10 +0100

    Create RequiredArgument.py

---
+++ RequiredArgument.py
@@ -0,0 +1,12 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+#required argument
+def f1(a1,a2):# two arguments gives
+    print(a1,type(a1))
+    print(a2,type(a2))
+
+f1(10,20)# both values are int
+f1(100,3.433)# a1 is int and a2 is float
+f1('',[])# it print out empty string and list
