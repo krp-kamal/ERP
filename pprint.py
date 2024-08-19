3dbb3d6e43df610a9574a07d9f5908597be195b7
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-19 19:54:21 +0100

    Create pprint.py

---
+++ pprint.py
@@ -0,0 +1,15 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+import pprint
+ICR = {
+    'ICR_NO': [],
+    'Sender_name': [],
+    'Subject': [],
+    'SenderDate': [],
+    'ReceivedBy': []
+}
+print(f"Type of ICR: {type(ICR)}")
+print(f"Keys in ICR: {ICR.keys()}")
+pprint.pprint(ICR)
