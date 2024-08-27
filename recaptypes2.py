a1f3b65bfa861ab859ab0919a0a0fd2d2618de9f
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-27 20:48:39 +0100

    Create recaptypes2.py

---
+++ recaptypes2.py
@@ -0,0 +1,18 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+a, b, c = 5, 10.5, "Hello"
+
+print(f"The type of a is: {type(a)}")   
+print(f"The type of b is: {type(b)}")   
+print(f"The type of c is: {type(c)}")  
+
+sum_of_ab = a + b
+concatenation = c + " World"
+
+
+print("Sum of a and b:", sum_of_ab)        
+print("Concatenated string:", concatenation)  
+
+
