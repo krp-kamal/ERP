9116166d4e050d0a65a2cae14b97048932c8ba96
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-28 21:05:38 +0100

    Create UserInput.py

---
+++ UserInputandArithmeticOperators.py
@@ -0,0 +1,13 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+# Calculate the average
+average = (num1 + num2 + num3) / 3
+
+# The results
+print(f"The average of the three numbers is: {average}")
