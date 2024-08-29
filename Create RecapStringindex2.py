7f7e4c1f27467677b3af3c9de692fb8050c092ed
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-29 13:20:58 +0100

    Create Create RecapStringindex2.py

---
+++ Create RecapStringindex2.py
@@ -0,0 +1,24 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+text = "Hello, Python! Learning Python is fun."
+
+# String Indexing and Slicing
+first_char = text[0]
+last_char = text[-1]
+greeting = text[:5]
+language = text[7:13]
+
+# String Operations
+combined = greeting + ", " + language + " is great!"
+uppercase_text = text.upper()
+replaced_text = text.replace("Python", "Programming")
+
+print(first_char)
+print(last_char)
+print(greeting)
+print(language)
+print(combined)
+print(uppercase_text)
+print(replaced_text)
