3305eca2ceb27569e3857245d18b46e92219e310
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-29 16:05:43 +0100

    Create recapStringMembership.py

---
+++ recapStringMembership.py
@@ -0,0 +1,14 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+text = "string membership operator"
+
+python = "Python" in text       
+java = "Java" in text           
+  
+fun_present = "fun" not in text     
+
+print("Is 'Python' present in text?", python)
+print("Is 'Java' present in text?", java)
+print("Is 'fun' not present in text?", fun_present)
