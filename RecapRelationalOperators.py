9cbfcb324fbb43488614ebdce1136e19a34c0504
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-29 15:06:14 +0100

    Create RecapRelationalOperators.py

---
+++ RecapRelationalOperators.py
@@ -0,0 +1,20 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+x = 10
+y = 20
+
+greater_than = x > y      
+less_than = x < y         
+equal_to = x == y         
+not_equal_to = x != y     
+greater_or_equal = x >= y 
+less_or_equal = x <= y    
+
+print(greater_than)
+print(less_than)
+print(equal_to)
+print(not_equal_to)
+print(greater_or_equal)
+print(less_or_equal)
