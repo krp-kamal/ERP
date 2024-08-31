8e608c78888fe9e3dfe53db07af0a6b60c1690c1
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-31 16:48:35 +0100

    Create RecapList2.py

---
+++ RecapList2.py
@@ -0,0 +1,13 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+numbers = [10, 20, 30, 40, 50, 60]
+
+# Slicing the list
+print(numbers[1:4])  
+
+print(numbers[:])
+numbers.append(70)
+print(numbers) 
+  
