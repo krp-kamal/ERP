7faa5992c968cd122f38c75738319183b1c755e3
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-31 16:42:45 +0100

    Create RecapList.py

---
+++ RecapList.py
@@ -0,0 +1,24 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+fruits = ['apple', 'banana', 'cherry']
+
+'''
+index
+'''
+print(fruits[0])  
+print(fruits[1])  
+
+'''
+ Modifying
+'''
+fruits[2] = 'orange'
+print(fruits)
+
+'''
+Appending
+'''
+
+fruits.append('grape')
+print(fruits)
