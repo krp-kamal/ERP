f59da9b9249e7bf4dc06a0cf13f406f31dba69e7
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-31 18:18:18 +0100

    Create ListMethodsRecap.py

---
+++ ListMethodsRecap.py
@@ -0,0 +1,16 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+numbers = [5, 2, 9, 1, 5, 6]
+
+# Sorting the list
+numbers.sort()
+print(numbers)  
+
+# Reversing (In Order)
+numbers.reverse()
+print(numbers)
+
+count_of_fives = numbers.count(5)
+print(count_of_fives) 
