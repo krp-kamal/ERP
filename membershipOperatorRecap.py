7b5e4ba9ecff2bf88ac0cdbef556e1f636ce4f47
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-31 18:16:40 +0100

    Create membershipOperatorRecap.py

---
+++ membershipOperatorRecap.py
@@ -0,0 +1,12 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+colour = ['red', 'green', 'blue']
+
+'''
+Checking if an item exists in the list
+'''
+print('red' in colour)  
+print('yellow' in colour)  
+print('yellow' not in colour) 
