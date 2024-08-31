5453abaff1d7dfd6b91b08fcc6c02cea84bc185d
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-08-31 18:35:57 +0100

    Create DictRecap.py

---
+++ DictRecap.py
@@ -0,0 +1,16 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+student = {'name': 'John Doe', 'age': 20,'major': 'Computer Science'}
+
+print(student['name'])  
+print(student['age'])   
+print(student)
+
+# Modifying dictionary values
+student['age'] = 21
+student['grade'] = 'A'
+
+print(student)  
+    
