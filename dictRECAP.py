093726a26b10de54556fabd6b874799e96691ca3
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-09 20:39:07 +0100

    Create dictRECAP.py

---
+++ dictRECAP.py
@@ -0,0 +1,8 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+fruits = {'apple': 5, 'banana': 3, 'cherry': 7}
+
+for fruit, quantity in fruits.items():
+    print(f"Fruit: {fruit}, Quantity: {quantity}")
