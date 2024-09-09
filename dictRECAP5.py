fe89dca590ba2e3ff849eab35e065d2fab2a52b1
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-09 21:45:48 +0100

    Create dictRECAP5.py

---
+++ dictRECAP5.py
@@ -0,0 +1,13 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+membership_codes = {
+    'AB123': 'Alice',
+    'CD456': 'Bob',
+    'EF789': 'Charlie'
+}
+
+for code, name in membership_codes.items():
+    print(f'Code {code} belongs to {name}')
+
