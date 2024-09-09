440905f55db2706ab0d42d1dbfef552507e57b3a
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-09 21:58:44 +0100

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,16 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+book_collection = set()
+
+book_collection.add('Initialize an empty set for books')
+book_collection.add('1984')
+book_collection.add('Add individual books to the collection')
+print("Book collection after adding books:", book_collection)
+
+book_collection.update({'The Great Gatsby', 'Moby Dick', 'War and Peace'})
+print("Book collection after updating with more books:", book_collection)
+
+book_collection.remove('1984')
+print("Book collection after removing a book:", book_collection)
