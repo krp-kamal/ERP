d9d09c13f9073d0d23484b0e17408d47d02cb29f
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-09 20:18:28 +0100

    Create listRECAP3.py

---
+++ listRECAP3.py
@@ -0,0 +1,10 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+
+colours = ["red", "blue", "green", "yellow"]
+index_number = colours.index("blue") 
+
+print(colours)
+print(f"The first 'blue' is at index: {index_number}")
