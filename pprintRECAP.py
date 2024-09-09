03e2a57f480a5d4ec4d8f85f9f171d9b7d372a3d
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-09 20:13:32 +0100

    Create pprintRECAP.py

---
+++ pprintRECAP.py
@@ -0,0 +1,15 @@
+'''
+Author: Ms Rya
+Version: 3.10
+'''
+
+from pprint import pprint
+
+data = [
+    {'Person ID': i, 'Person Name': 'Delwin', 'Person City': 'Chennai'}
+    for i in range(5, 27)
+]
+
+print('Data entered successfully.')
+
+pprint(data)
