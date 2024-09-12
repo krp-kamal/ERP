9c7d09553df3c541a37e8ba5be25e3acc17ed997
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-12 15:35:28 +0100

    Update ArithmeticOperations.py

--- ArithmeticOperations.py
+++ ArithmeticOperations.py
@@ -2,20 +2,21 @@
 Author: Ms Rya
 Version: 3.10
 '''
-# Take input from the user
+def arithmeticOperations(num_str):
+    num_int = int(num_str)
+    num_float = float(num_str)
+
+    sum_result = num_int + 10
+    product_result = num_float * 2.5
+    input_length = len(num_str)
+
+    print("Results of arithmetic operations:")
+    print(f"Integer + 10: {sum_result}")
+    print(f"Float * 2.5: {product_result}")
+    print(f"Length of the input string: {input_length}")
+
 num_str = input("Enter a number: ")
 
-num_int = int(num_str)
-num_float = float(num_str)
+arithmeticOperations(num_str)
 
-
-sum_result = num_int + 10
-product_result = num_float * 2.5
-input_length = len(num_str)
-
-# Display results
-print("Results of arithmetic operations:")
-print(f"Integer + 10: {sum_result}")
-print(f"Float * 2.5: {product_result}")
-print(f"Length of the input string: {input_length}")
 
