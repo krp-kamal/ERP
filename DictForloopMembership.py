45793b9f7d3ef305e7152882b791c6070ca12348
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 14:33:45 +0100

    Update DictForloopMembership.py

--- DictForloopMembership.py
+++ DictForloopMembership.py
@@ -2,15 +2,18 @@
 Author: Ms Rya
 Version: 3.10
 '''
-server_details={} 
 
-K = input("Enter an alias name:")  
-V = input("Enter an IP-Address:")  
-server_details[K] = V  # Add the alias name (K) and IP address (V) as a key-value pair to the dictionary
+def update_server_details():
+    server_details = {}
+    K = input("Enter an alias name:")  
+    V = input("Enter an IP-Address:")  
+    server_details[K] = V
 
-print("Alias:{}\t IP-Address:{}".format(K, server_details[K]))  #Print the alias name and its associated IP address
+    print("Alias:{}\t IP-Address:{}".format(K, server_details[K]))
 
-server_details[K] = "12456"  #  Change the IP address associated with the alias name to "127.0.0.1"
+    server_details[K] = "12456"
 
-print("Updated_dict")  # Indicate that the dictionary has been updated
-print("Alias:{}\t IP-Address:{}".format(K, server_details[K]))  # Print the alias name and its new associated IP address
+    print("Updated_dict")
+    print("Alias:{}\t IP-Address:{}".format(K, server_details[K]))
+
+update_server_details()
