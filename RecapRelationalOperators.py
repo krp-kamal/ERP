33d35eca1bd2cd37b320ef7b1fab60d2224cccd0
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:20:30 +0100

    Update RecapRelationalOperators.py

--- RecapRelationalOperators.py
+++ RecapRelationalOperators.py
@@ -2,19 +2,23 @@
 Author: Ms Rya
 Version: 3.10
 '''
+
+def compare_numbers(x, y):
+    greater_than = x > y      
+    less_than = x < y         
+    equal_to = x == y         
+    not_equal_to = x != y     
+    greater_or_equal = x >= y 
+    less_or_equal = x <= y    
+
+    print("Greater than:", greater_than)
+    print("Less than:", less_than)
+    print("Equal to:", equal_to)
+    print("Not equal to:", not_equal_to)
+    print("Greater or equal:", greater_or_equal)
+    print("Less or equal:", less_or_equal)
+
+
 x = 10
 y = 20
-
-greater_than = x > y      
-less_than = x < y         
-equal_to = x == y         
-not_equal_to = x != y     
-greater_or_equal = x >= y 
-less_or_equal = x <= y    
-
-print(greater_than)
-print(less_than)
-print(equal_to)
-print(not_equal_to)
-print(greater_or_equal)
-print(less_or_equal)
+compare_numbers(x, y)
