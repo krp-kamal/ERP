a69a236df8c04a23ec339e28e2b95d85bd9a89a7
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 14:34:42 +0100

    Update DictMethodRecap.py

--- DictMethodRecap.py
+++ DictMethodRecap.py
@@ -2,10 +2,13 @@
 Author: Ms Rya
 Version: 3.10
 '''
+
+def person_info(person):
+    removed_age = person.pop('age')
+    print(f"Removed Age: {removed_age}\nUpdated Person: {person}")
+
+    person.update(location='New York', age=31)
+    print("New Info:", person)
+
 person = {'name': 'Jane Doe', 'age': 30, 'job': 'Engineer'}
-
-removed_age = person.pop('age')
-print(f"Removed Age: {removed_age}\nUpdated Person: {person}")
-
-person.update(location='New York', age=31)
-print("New Info:", person)
+person_info(person)
