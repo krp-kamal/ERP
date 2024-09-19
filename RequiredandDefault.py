e5a99445272f4107bd7c10e3ba52f257fda9875c
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:22:16 +0100

    Update RequiredandDefault.py

--- RequiredandDefault.py
+++ RequiredandDefault.py
@@ -2,12 +2,11 @@
 Author: Ms Rya
 Version: 3.10
 '''
-d = {}  # Create an empty dictionary
-# Since a1 is not in the dictionary, it will be added with the value 'a2'
-d.setdefault('a1', 'a2')
-# Use setdefault to ensure 'a1' is in the dictionary with a default value of 'a2'
-#'a3' is not in the dictionary and no default value is provided, it will be added with the value None
-d.setdefault('a3')
-# Use setdefault to ensure 'a3' is in the dictionary with a default value of None beacause there is no value
-print(d)  
 
+def manage_dictionary():
+    d = {}
+    d.setdefault('a1', 'a2')
+    d.setdefault('a3')
+    print(d)
+
+manage_dictionary()
