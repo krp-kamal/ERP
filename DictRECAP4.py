dde0087e8c714a63d875a6fc59abde6c0fdbb451
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 14:35:57 +0100

    Update DictRECAP4.py

--- DictRECAP4.py
+++ DictRECAP4.py
@@ -2,6 +2,13 @@
 Author: Ms Rya
 Version: 3.10
 '''
+
+def f1_membership_code(membership_codes, code_to_check):
+    if code_to_check in membership_codes:
+        print(f'Code {code_to_check} belongs to {membership_codes[code_to_check]}')
+    else:
+        print(f'Code {code_to_check} not found.')
+
 membership_codes = {
     'AB123': 'Alice',
     'CD456': 'Bob',
@@ -9,8 +16,5 @@ membership_codes = {
 }
 
 code_to_check = 'CD456'
+f1_membership_code(membership_codes, code_to_check)
 
-if code_to_check in membership_codes:
-    print(f'Code {code_to_check} belongs to {membership_codes[code_to_check]}')
-else:
-    print(f'Code {code_to_check} not found.')
