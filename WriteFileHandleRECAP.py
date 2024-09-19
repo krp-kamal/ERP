f90c53e111bd7f97f0867998c790b9fd61b63ed7
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:28:54 +0100

    Update WriteFileHandleRECAP.py

--- WriteFileHandleRECAP.py
+++ WriteFileHandleRECAP.py
@@ -2,10 +2,12 @@
 Author: Ms Rya
 Version: 3.10
 '''
-filename = 'example2.log'
+
+def writelines_file(filename, lines):
+    with open(filename, 'w') as file:
+        file.writelines(lines)
+    print(f"Lines have been written to '{filename}'.")
+
+filename = '/Users/kamalperumal/Downloads/example2.log'
 lines_to_write = ['Line 1\n', 'Line 2\n', 'Line 3\n']
-
-with open(filename, 'w') as file:
-    file.writelines(lines_to_write)
-
-print(f"Lines have been written to '{filename}'.")
+writelines_file(filename, lines_to_write)
