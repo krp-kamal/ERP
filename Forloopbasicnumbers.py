32eba7c82369513669e04c69ab65e4b8fd9a1c2c
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:03:32 +0100

    Update Forloopbasicnumbers.py

--- Forloopbasicnumbers.py
+++ Forloopbasicnumbers.py
@@ -2,5 +2,11 @@
 Author: Ms Rya
 Version: 3.10
 '''
-for i in range(5):
-    print(i)
+
+def print_numbers(n):
+    print(f"Printing numbers from 0 to {n}:")
+    for i in range(n):
+        print(i)
+
+number_to_print = 5
+print_numbers(number_to_print)
