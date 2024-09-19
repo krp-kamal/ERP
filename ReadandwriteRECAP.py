beb72e1e55e6835b424b3ac59e99e819aad38311
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:17:03 +0100

    Update ReadandwriteRECAP.py

--- ReadandwriteRECAP.py
+++ ReadandwriteRECAP.py
@@ -2,18 +2,16 @@
 Author: Ms Rya
 Version: 3.10
 '''
+
+def file_content(source_file, destination_file):
+    with open(source_file, 'r') as src:
+        content = src.read()
+
+    with open(destination_file, 'w') as dest:
+        dest.write(content)
+
+    print(f"Content from '{source_file}' has been copied to '{destination_file}'.")
+
 source_file = '/Users/kamalperumal/Downloads/example.log'
 destination_file = '/Users/kamalperumal/Downloads/DFile.log'
-
-with open(source_file, 'r') as src:
-    content = src.read()
-'''
-Read Entire existing File
-'''
-with open(destination_file, 'w') as dest:
-    dest.write(content)
-'''
-Write it in a new File
-'''
-print(f"Content from '{source_file}' has been copied to '{destination_file}'.")
-
+file_content(source_file, destination_file)
