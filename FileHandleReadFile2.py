9b3e6799af6686cd59b0157c3b3e65193da467cf
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 15:08:08 +0100

    Update FileHandleReadFile2.py

--- FileHandleReadFile2.py
+++ FileHandleReadFile2.py
@@ -2,7 +2,16 @@
 Author: Ms Rya
 Version: 3.10
 '''
-FH= open('/Users/kamalperumal/Downloads/FileHandleRead.py')
-for var in FH.readlines()[-2]:
-    print(var.strip())
-FH.close()
+
+def last_line(file_path):
+    with open(file_path, 'r') as FH:
+        lines = FH.readlines()
+        if len(lines) >= 2:
+            second_last_line = lines[-2]
+            for var in second_last_line:
+                print(var.strip())
+        else:
+            print("The file does not have enough lines.")
+
+file_path = '/Users/kamalperumal/Downloads/FileHandleRead.py'
+last_line(file_path)
