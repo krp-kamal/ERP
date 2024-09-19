ea236c7c1823e80e50a28db35b67ab5a59132d04
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 14:36:37 +0100

    Update DictRecap.py

--- DictRecap.py
+++ DictRecap.py
@@ -2,15 +2,16 @@
 Author: Ms Rya
 Version: 3.10
 '''
-student = {'name': 'John Doe', 'age': 20,'major': 'Computer Science'}
 
-print(student['name'])  
-print(student['age'])   
-print(student)
+def update_student_info(student):
+    print(student['name'])
+    print(student['age'])
+    print(student)
 
-# Modifying dictionary values
-student['age'] = 21
-student['grade'] = 'A'
+    student['age'] = 21
+    student['grade'] = 'A'
 
-print(student)  
-    
+    print(student)
+
+student = {'name': 'John Doe', 'age': 20, 'major': 'Computer Science'}
+update_student_info(student)
