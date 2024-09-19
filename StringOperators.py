c3aa697707d403eba2af4585c441ec02461fb0be
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:24:24 +0100

    Update StringOperators.py

--- StringOperators.py
+++ StringOperators.py
@@ -2,11 +2,23 @@
 Author: Ms Rya
 Version: 3.10
 '''
-s1 = 'python'
-s2 = 'programming:'
-s3 = '\tdata\t'
-s4 = '\nsamplecode\n'
-print(s1.strip())
-print(s2.strip(:))
-print(s3.strip())
-print(s4.strip())
+
+def strip_strings():
+    s1 = 'python'
+    s2 = 'programming:'
+    s3 = '\tdata\t'
+    s4 = '\nsamplecode\n'
+    
+    stripped_s1 = s1.strip()
+    stripped_s2 = s2.strip(':')
+    stripped_s3 = s3.strip()
+    stripped_s4 = s4.strip()
+    
+    return stripped_s1, stripped_s2, stripped_s3, stripped_s4
+
+def main():
+    stripped_results = strip_strings()
+    for result in stripped_results:
+        print(result)
+
+main()
