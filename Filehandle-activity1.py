8684fc9af8d8eccb9414386a38e2205f78a7d10f
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 15:13:46 +0100

    Update Filehandle-activity1.py

--- Filehandle-activity1.py
+++ Filehandle-activity1.py
@@ -2,9 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
-robj = open("/Users/kamalperumal/Downloads/r1.log", 'r')
-wobj = open("/Users/kamalperumal/Downloads/r2.log" , 'w')
-s = robj.read()
-wobj.write(s)
-robj.close()
-wobj.close()
+
+def file_contents(source_file_path, destination_file_path):
+    with open(source_file_path, 'r') as robj:
+        contents = robj.read()
+    
+    with open(destination_file_path, 'w') as wobj:
+        wobj.write(contents)
+
+source_file_path = "/Users/kamalperumal/Downloads/r1.log"
+destination_file_path = "/Users/kamalperumal/Downloads/r2.log"
+file_contents(source_file_path, destination_file_path)
