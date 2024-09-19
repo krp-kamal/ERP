ce73dfcbc50038ef8580a2e3f40b6b72aae8b4e1
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 13:48:53 +0100

    Update AppendFileHandle.py

--- AppendFileHandle.py
+++ AppendFileHandle.py
@@ -2,11 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
-robj= open ("/Users/kamalperumal/Downloads/FileHandleRead.py",'r')
-wobj = open ("/Users/kamalperumal/Downloads/Read3.log",'w')
-s = robj.read()
-wobj.write(s)
-wobj.write('genny\n')
-wobj.write('April\n')
-robj.close()
-wobj.close()
+def copy_file(read_file, write_file):
+    with open(read_file, 'r') as robj:
+        with open(write_file, 'w') as wobj:
+            s = robj.read()
+            wobj.write(s)
+            wobj.write('genny\n')
+            wobj.write('April\n')
+read_file = "/Users/kamalperumal/Downloads/FileHandleRead.py"
+write_file = "/Users/kamalperumal/Downloads/Read3.log"
+copy_file(read_file, write_file)
+
