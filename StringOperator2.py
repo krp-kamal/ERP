0082bcc733fbc9f6adfa784098ec219608fd13d0
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:23:43 +0100

    Update StringOperator2.py

--- StringOperator2.py
+++ StringOperator2.py
@@ -2,5 +2,16 @@
 Author: Ms Rya
 Version: 3.10
 '''
-s= ' a b c d ef g'
-print(s.count(' '))
+
+def count_spaces(input_string):
+    space_count = input_string.count(' ')
+    return space_count
+
+def main():
+    s = ' a b c d ef g'
+    print("Original string:", repr(s))
+    
+    space_count = count_spaces(s)
+    print("Number of spaces in the string:", space_count)
+
+main()
