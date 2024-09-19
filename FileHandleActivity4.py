cd06d0e753798706537ee229e89c48ed541c9b85
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 14:58:17 +0100

    Update FileHandleActivity4.py

--- FileHandleActivity4.py
+++ FileHandleActivity4.py
@@ -2,7 +2,16 @@
 Author: Ms Rya
 Version: 3.10
 '''
-# reads the entire file #
-robj= open('/Users/kamalperumal/Downloads/r3.log','r')
-print(robj.read())
-robj.close()
+
+def read_file(file_path, num_chars=None):
+    with open(file_path, 'r') as robj:
+        if num_chars is not None:
+            contents = robj.read(num_chars)
+        else:
+            contents = robj.read()
+    
+    print(contents)
+
+file_path = '/Users/kamalperumal/Downloads/r3.log'
+read_file(file_path)  
+read_file(num_chars=50)  
