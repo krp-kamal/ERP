a43e56b1905ccfd5b353f00fbf6b1bad71d4bde2
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 15:17:46 +0100

    Update FileHandleWrite.py

--- FileHandleWrite.py
+++ FileHandleWrite.py
@@ -2,9 +2,12 @@
 Author: Ms Rya
 Version: 3.10
 '''
-wobj= open('/Users/kamalperumal/Downloads/Writefile.log', 'w')
-wobj.write('neil\n')
-wobj.write('Tom\n')
-wobj.write('zack\n')
-wobj.write('shan\n')
-wobj.close()
+
+def write_default_names_to_file(file_path):
+    names = ['neil', 'Tom', 'zack', 'shan']
+    with open(file_path, 'w') as wobj:
+        for name in names:
+            wobj.write(name + '\n')
+
+file_path = '/Users/kamalperumal/Downloads/Writefile.log'
+write_default_names_to_file(file_path)
