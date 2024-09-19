ebdbdb79d1a34634e6a7906ea46d6e0a27d8f130
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:04:22 +0100

    Update Forloopbasicsdict.py

--- Forloopbasicsdict.py
+++ Forloopbasicsdict.py
@@ -2,6 +2,11 @@
 Author: Ms Rya
 Version: 3.10
 '''
-User = {'Name': 'Hailey', 'Age': 30, 'City': 'New York'}
-for key, value in User.items():
-    print(f'{key}: {value}')
+
+def print_user_details(user_info):
+    print("User Details:")
+    for key, value in user_info.items():
+        print(f'{key}: {value}')
+
+user = {'Name': 'Hailey', 'Age': 30, 'City': 'New York'}
+print_user_details(user)
