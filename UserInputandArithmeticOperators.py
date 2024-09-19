b032d3092432a3e2f086a908deb022e5f6b474e5
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:25:29 +0100

    Update UserInputandArithmeticOperators.py

--- UserInputandArithmeticOperators.py
+++ UserInputandArithmeticOperators.py
@@ -2,12 +2,19 @@
 Author: Ms Rya
 Version: 3.10
 '''
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-num3 = int(input("Enter the third number: "))
 
-# Calculate the average
-average = (num1 + num2 + num3) / 3
+def calculate_average():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    num3 = int(input("Enter the third number: "))
 
-# The results
-print(f"The average of the three numbers is: {average}")
+    total = num1 + num2 + num3
+    average = total / 3
+
+    print(f"The sum of the numbers is: {total}")
+    print(f"The average of the three numbers is: {average}")
+
+def main():
+    calculate_average()
+
+main()
