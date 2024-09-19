644f5a67f597c5c1180a20c3ac7a1e859f3c332d
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:15:41 +0100

    Update Read&WriteFileHandle.py

--- Read&WriteFileHandle.py
+++ Read&WriteFileHandle.py
@@ -2,23 +2,22 @@
 Author: Ms Rya
 Version: 3.10
 '''
-#collects information from the user#
-Name = input('Enter your name: ')
-Age = input('Enter your age: ')
-Age = int(Age)
-Height = input('How tall are you (cm): ')
-Height = int(Height)
-File_Name = input('Enter the file name you need to save: ')
-#writes the collected info#
-with open('Writefile2.txt', 'w') as file:
-    file.write('File Name: ' + File_Name + '\n')
-    file.write('Details about the user:\n')
-    file.write('Name: ' + Name + '\n')
-    file.write('Age: ' + str(Age) + '\n')
-    file.write('Height: ' + str(Height) + ' cm\n')
-    file.close()
-#Read the collected info#
-with open('Writefile2.txt', 'r') as file:
-    content = file.read()
-    print(content) 
-    file.close()
+
+def user_info():
+    Name = input('Enter your name: ')
+    Age = int(input('Enter your age: '))
+    Height = int(input('How tall are you (cm): '))
+    File_Name = input('Enter the file name you need to save: ')
+
+    with open(File_Name, 'w') as file:
+        file.write('File Name: ' + File_Name + '\n')
+        file.write('Details about the user:\n')
+        file.write('Name: ' + Name + '\n')
+        file.write('Age: ' + str(Age) + '\n')
+        file.write('Height: ' + str(Height) + ' cm\n')
+
+    with open(File_Name, 'r') as file:
+        content = file.read()
+        print(content)
+
+user_info()
