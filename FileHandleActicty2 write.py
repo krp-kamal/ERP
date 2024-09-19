5e847feda9bcc1e6a898ad448db4634416e5d69d
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 14:53:43 +0100

    Update FileHandleActicty2 write.py

--- FileHandleActicty2 write.py
+++ FileHandleActicty2 write.py
@@ -2,10 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
-wopj= open("/Users/kamalperumal/Downloads/r1.log","w")
-wopj.write('Test server1\n')
-wopj.write('Test server2\n')
-wopj.write('Test server3\n')
-wopj.write('Test server4\n')
-wopj.write('Test server5\n')
-wopj.close()
+
+def write_file(file_path):
+    with open(file_path, "w") as wopj:
+        wopj.write('Test server1\n')
+        wopj.write('Test server2\n')
+        wopj.write('Test server3\n')
+        wopj.write('Test server4\n')
+        wopj.write('Test server5\n')
+
+file_path = "/Users/kamalperumal/Downloads/r1.log"
+write_file(file_path)
