f119db490ac0328993ff5f901a41de085115d23c
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:13:22 +0100

    Update NestedconditionRecap.py

--- NestedconditionRecap.py
+++ NestedconditionRecap.py
@@ -2,14 +2,16 @@
 Author: Ms Rya
 Version: 3.10
 '''
-age = int(input("Enter your age: "))
 
-if age >= 18:
-    print("You are an adult.")
-    '''
-    Nested condition
-    '''
+def check_age():
+    age = int(input("Enter your age: "))
+
     if age >= 18:
-        print("You are eligible to vote.")
-else:
-    print("You are a minor. However, you are not eligible to vote")
+        print("You are an adult.")
+        if age >= 18:
+            print("You are eligible to vote.")
+    else:
+        print("You are a minor. However, you are not eligible to vote.")
+
+
+check_age()
