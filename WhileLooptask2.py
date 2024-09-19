7e00a08a37c75dd6b1c572c3b9f8a0ff3b966439
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:27:37 +0100

    Update WhileLooptask2.py

--- WhileLooptask2.py
+++ WhileLooptask2.py
@@ -2,13 +2,17 @@
 Author: Ms Rya
 Version: 3.10
 '''
-# No limits to the loop
-while True:
-    name = input('Enter the login name:')  # if name is 'apple' the loop terminates
-    if name == 'apple':
-        print('You have logged in!')
-        break
-    else:
-        print('Failed! Try again.')# else it will run multiple times until succeeded
 
-    
+def login():
+    while True:
+        name = input('Enter the login name: ')
+        if name == 'apple':
+            print('You have logged in!')
+            break
+        else:
+            print('Failed! Try again.')
+
+def main():
+    login()
+
+main()
