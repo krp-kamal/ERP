2ecd1d77f42ed28142b9750ecfcb58336951c151
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 14:28:42 +0100

    Update Create RecapStringindex2.py

--- Create RecapStringindex2.py
+++ Create RecapStringindex2.py
@@ -2,23 +2,25 @@
 Author: Ms Rya
 Version: 3.10
 '''
+
+def manipulate_string(text):
+    first_char = text[0]
+    last_char = text[-1]
+    greeting = text[:5]
+    language = text[7:13]
+
+    combined = greeting + ", " + language + " is great!"
+    uppercase_text = text.upper()
+    replaced_text = text.replace("Python", "Programming")
+
+    # Print results
+    print(first_char)
+    print(last_char)
+    print(greeting)
+    print(language)
+    print(combined)
+    print(uppercase_text)
+    print(replaced_text)
+
 text = "Hello, Python! Learning Python is fun."
-
-# String Indexing and Slicing
-first_char = text[0]
-last_char = text[-1]
-greeting = text[:5]
-language = text[7:13]
-
-# String Operations
-combined = greeting + ", " + language + " is great!"
-uppercase_text = text.upper()
-replaced_text = text.replace("Python", "Programming")
-
-print(first_char)
-print(last_char)
-print(greeting)
-print(language)
-print(combined)
-print(uppercase_text)
-print(replaced_text)
+manipulate_string(text)
