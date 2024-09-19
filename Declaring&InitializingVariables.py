6fdf9464214a1a0b2cdfa0fe8f0f6e0ab7661cae
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 14:30:09 +0100

    Update Declaring&InitializingVariables.py

--- Declaring&InitializingVariables.py
+++ Declaring&InitializingVariables.py
@@ -2,16 +2,24 @@
 Author: Ms Rya
 Version: 3.10
 '''
+
+def print_file_info(filedetails, filenames, filesize, fileindexnumber):
+    print('File details:', filedetails)
+    print(type(filedetails))
+    
+    print('File names:', filenames)
+    print(type(filenames))
+    
+    print('File size:', filesize)
+    print(type(filesize))
+    
+    print('File Index Number:', fileindexnumber)
+    print(type(fileindexnumber))
+
 filedetails = 'consists of users data'
 filenames = 'Filename.rog'
 filesize = 104
 fileindexnumber = 23
 
-print('File deatils:', filedetails)
-print(type(filedetails))
-print('File names:', filenames)
-print(type(filenames))
-print('File size:', filesize)
-print(type(filesize))
-print('File Index Number:', fileindexnumber)
-print(type(fileindexnumber))
+print_file_info(filedetails, filenames, filesize, fileindexnumber)
+
