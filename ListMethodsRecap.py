77360e00e8e1ce519c1190e857ae61b338bc605e
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:10:32 +0100

    Update ListMethodsRecap.py

--- ListMethodsRecap.py
+++ ListMethodsRecap.py
@@ -2,15 +2,16 @@
 Author: Ms Rya
 Version: 3.10
 '''
-numbers = [5, 2, 9, 1, 5, 6]
 
-# Sorting the list
-numbers.sort()
-print(numbers)  
+def process_numbers(numbers, number_to_count):
+    numbers.sort()
+    print("Sorted numbers:", numbers)
+    
+    numbers.reverse()
+    print("Reversed numbers:", numbers)
+    
+    count = numbers.count(number_to_count)
+    print(f"Count of {number_to_count}:", count)
 
-# Reversing (In Order)
-numbers.reverse()
-print(numbers)
-
-count_of_fives = numbers.count(5)
-print(count_of_fives) 
+numbers_list = [5, 2, 9, 1, 5, 6]
+process_numbers(numbers_list, 5)
