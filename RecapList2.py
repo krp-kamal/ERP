a56e8cf4c34dc7b9dc1b7314481a20ceec6c5f79
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:18:35 +0100

    Update RecapList2.py

--- RecapList2.py
+++ RecapList2.py
@@ -2,12 +2,20 @@
 Author: Ms Rya
 Version: 3.10
 '''
-numbers = [10, 20, 30, 40, 50, 60]
 
-# Slicing the list
-print(numbers[1:4])  
+def manage_numbers():
+    numbers = [10, 20, 30, 40, 50, 60]
 
-print(numbers[:])
-numbers.append(70)
-print(numbers) 
+    # Slicing the list
+    print("Sliced numbers (index 1 to 4):")
+    print(numbers[1:4])  
+
+    print("All numbers:")
+    print(numbers[:]) 
   
+    # Appending a new number
+    numbers.append(70)
+    print("After appending 70:")
+    print(numbers) 
+
+manage_numbers()
