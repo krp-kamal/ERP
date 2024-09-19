323f8fdf89ab0096f4524b5b6c30b2143b12c21d
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:20:00 +0100

    Update RecapReadAppened.py

--- RecapReadAppened.py
+++ RecapReadAppened.py
@@ -2,15 +2,19 @@
 Author: Ms Rya
 Version: 3.10
 '''
-filename = 'example.log'
-Append_Text = 'Additional text.'
 
-with open(filename, 'r') as file:
-    content = file.read()
+def append_text(filename, append_text, search_text):
+    with open(filename, 'r') as file:
+        content = file.read()
 
-if 'Line 1' in content:
-    with open(filename, 'a') as file:
-        file.write(Append_Text)
-    print(f"Text '{Append_Text}' has been appended.")
-else:
-    print("The specific text was not found. Nothing was appended.")
+    if search_text in content:
+        with open(filename, 'a') as file:
+            file.write(append_text)
+        print(f"Text '{append_text}' has been appended.")
+    else:
+        print("The specific text was not found. Nothing was appended.")
+
+filename = '/Users/kamalperumal/Downloads/example.log'
+append_text = 'Additional text.'
+search_text = 'Line 1'
+append_text(filename, append_text, search_text)
