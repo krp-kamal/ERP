a2ac1235bea290bde1c7a02c9bd0b9ddb7f5ec1b
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:16:25 +0100

    Update ReadandLen.py

--- ReadandLen.py
+++ ReadandLen.py
@@ -3,14 +3,16 @@ Author: Ms Rya
 Version: 3.10
 '''
 
-filename = '/Users/kamalperumal/Downloads/FileHandleRead.py'
-
-with open(filename, 'r') as file:
-    lines = file.readlines()
+def last_line(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
     
-if len(lines) >= 2:
-    second_last_line = lines[-2].strip()
-    print(second_last_line)
-else:
-    print("The file does not have enough lines.")
+    if len(lines) >= 2:
+        second_last_line = lines[-2].strip()
+        print(second_last_line)
+    else:
+        print("The file does not have enough lines.")
 
+
+filename = '/Users/kamalperumal/Downloads/FileHandleRead.py'
+last_line(filename)
