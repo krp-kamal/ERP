341811727c7ee29271c37c7d360e92b11dccbd5f
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:25:04 +0100

    Update TypecastBasics.py

--- TypecastBasics.py
+++ TypecastBasics.py
@@ -2,48 +2,45 @@
 Author: Ms Rya
 Version: 3.10
 '''
-# Integer to String#
-num = 30
-num_str = str(num)
-print(num_str)
 
-# String to Integer
-num_str = '10'
-num = int(num_str)
-print(num)
+def type_conversion():
+    num = 30
+    num_str = str(num)
+    print("Integer to String:", num_str)
 
-# String to Character
-char_str = 'A'
-char = char_str[0]
-print(char)
+    num_str = '10'
+    num = int(num_str)
+    print("String to Integer:", num)
 
-# Character to String
-char = 'A'
-char_str = str(char)
-print(char_str) 
+    char_str = 'A'
+    char = char_str[0]
+    print("String to Character:", char)
 
-# Integer to Boolean
-num = 1
-bool_val = bool(num)
-print(bool_val)  
+    char = 'A'
+    char_str = str(char)
+    print("Character to String:", char_str)
 
-# Boolean to Integer
-bool_val = True
-num = int(bool_val)
-print(num) 
+    num = 1
+    bool_val = bool(num)
+    print("Integer to Boolean:", bool_val)
 
-# Boolean to String
-bool_val = True
-str_val = str(bool_val)
-print(str_val) 
+    bool_val = True
+    num = int(bool_val)
+    print("Boolean to Integer:", num)
 
-# Float to Integer
-flt = 10.56
-num = int(flt)
-print(num)
+    bool_val = True
+    str_val = str(bool_val)
+    print("Boolean to String:", str_val)
 
-# Integer to Float
-num = 10
-flt = float(num)
-print(flt)  
+    flt = 10.56
+    num = int(flt)
+    print("Float to Integer:", num)
 
+    num = 10
+    flt = float(num)
+    print("Integer to Float:", flt)
+
+def main():
+    type_conversion()
+
+main()
