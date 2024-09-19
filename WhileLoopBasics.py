4e08790582aeb79d77726e8f298f62d7576c8891
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-19 18:26:44 +0100

    Update WhileLoopBasics.py

--- WhileLoopBasics.py
+++ WhileLoopBasics.py
@@ -2,8 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
-i = 0
-while i < 5:
-    i += 1
-    print(i)
-    
+
+def print_numbers():
+    i = 0
+    while i < 5:
+        i += 1
+        print(i)
+
+def main():
+    print_numbers()
+
+main()
