3cd221a9e91155958b8460872a564d93df413e44
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 16:21:58 +0100

    Update tryExpectFilehandle.py

--- tryExpectFilehandle.py
+++ tryExpectFilehandle.py
@@ -2,13 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
+def readfile(filename):
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+            print("File Content:")
+            print(content)
+    except FileNotFoundError:
+        print(f"The file '{filename}' does not exist.")
+        
 filename = 'RECAP.py'
-
-try:
-    with open(filename, 'r') as file:
-        content = file.read()
-        print("File Content:")
-        print(content)
-except FileNotFoundError:
-    print(f"The file '{filename}' does not exist.")
-
+readfile(filename)
