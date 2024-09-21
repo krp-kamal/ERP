55ffb69cc7674280c0018f9f74ed84d3b0aabea7
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 15:11:27 +0100

    Update recaptypes2.py

--- recaptypes2.py
+++ recaptypes2.py
@@ -2,17 +2,18 @@
 Author: Ms Rya
 Version: 3.10
 '''
-a, b, c = 5, 10.5, "Hello"
+def variable_operations(a, b, c):
+    print(f"The type of a is: {type(a)}")   
+    print(f"The type of b is: {type(b)}")   
+    print(f"The type of c is: {type(c)}")  
 
-print(f"The type of a is: {type(a)}")   
-print(f"The type of b is: {type(b)}")   
-print(f"The type of c is: {type(c)}")  
+    sum_of_ab = a + b
+    concatenation = c + " World"
 
-sum_of_ab = a + b
-concatenation = c + " World"
+    print("Sum of a and b:", sum_of_ab)        
+    print("Concatenated string:", concatenation)
 
-
-print("Sum of a and b:", sum_of_ab)        
-print("Concatenated string:", concatenation)  
-
-
+a = 5
+b = 10.5
+c = 'Hello'
+variable_operations(a, b, c)
