44d301c2ea464c842584216bc3af1bce7a0f7a8b
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 15:47:44 +0100

    Update sqlite3.py

--- sqlite3.py
+++ sqlite3.py
@@ -2,15 +2,19 @@
 Author: Ms Rya
 Version: 3.10
 '''
-import sqlite3 
-conobj = sqlite3.connect('test1.db')
-sth = conobj.cursor()
-'''
-sth.execute(' create table T3 (pid int, pname text)')
-'''
-sth.execute("insert into T3(pid, pname) values(101,'productA')")
-sth.execute("insert into T3(pid, pname) values(102,'productB')")
-sth.execute("insert into T3(pid, pname) values(103,'productC')")
-sth.execute('select * from T3')
-r = sth.fetchall()
-print(r)
+import sqlite3
+def Display():
+    conobj = sqlite3.connect('test1.db')
+    sth = conobj.cursor()
+    # sth.execute(' create table T3 (pid int, pname text)')
+
+    sth.execute("insert into T3(pid, pname) values(101,'productA')")
+    sth.execute("insert into T3(pid, pname) values(102,'productB')")
+    sth.execute("insert into T3(pid, pname) values(103,'productC')")
+
+    sth.execute('select * from T3')
+    r = sth.fetchall()
+    print(r)
+    conobj.close()
+
+Display()    
