9c6e43c7fbba2648a68cb71ef2b39574c19eaa6b
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:36:26 +0100

    Update listRECAP5.py

--- listRECAP5.py
+++ listRECAP5.py
@@ -2,10 +2,11 @@
 Author: Ms Rya
 Version: 3.10
 '''
+def sort_and_lowercase(words):
+    sorted_words = sorted(words, key=str.lower)
+    lowercase_words = [word.lower() for word in sorted_words]
+    return lowercase_words
+  
 words = ['BANANA', 'APPLE', 'CHERRY', 'DATE']
-
-sorted_words = sorted(words, key=str.lower)
-
-lowercase_words = [word.lower() for word in sorted_words]
-
-print(lowercase_words)
+result = sort_and_lowercase(words)
+print(result)
