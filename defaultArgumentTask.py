4a9283aba99458490bb2437f995bae730ba7397a
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:22:18 +0100

    Update defaultArgumentTask.py

--- defaultArgumentTask.py
+++ defaultArgumentTask.py
@@ -2,8 +2,15 @@
 Author: Ms Rya
 Version: 3.10
 '''
-fruits = [100,200,300,400]
-fruits.pop()
-print(fruits)
-fruits.pop(2)
-print(fruits)
+
+def modify_fruits(fruits):
+    fruits.pop()         # Remove the last item
+    print(fruits)
+    fruits.pop(2)        # Remove the item at index 2
+    print(fruits)
+
+# List of fruits
+fruits = [100, 200, 300, 400]
+
+# Call the function
+modify_fruits(fruits)
