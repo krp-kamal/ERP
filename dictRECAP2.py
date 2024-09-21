972c02e3c72cad0b21c826239888f84d64fb8205
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:30:26 +0100

    Update dictRECAP2.py

--- dictRECAP2.py
+++ dictRECAP2.py
@@ -2,7 +2,10 @@
 Author: Ms Rya
 Version: 3.10
 '''
+def student_score(scores, student_name):
+    if student_name in scores:
+        print(f"{student_name}'s score is:", scores[student_name])
+
 student_scores = {'John': 85, 'Emma': 92, 'Sam': 78}
 
-if 'Emma' in student_scores:
-    print("Emma's score is:", student_scores['Emma'])
+student_score(student_scores, 'Emma')
