7eaaf1703e0f865c1f8d82d158299063e780632c
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 16:10:07 +0100

    Update stringSlicing.py

--- stringSlicing.py
+++ stringSlicing.py
@@ -2,8 +2,12 @@
 Author: Ms Rya
 Version: 3.10
 '''
-S1 = 'root:x:bin:bash'
-print(S1[3])
-print(S1[-1])
-print(S1[-4:])
-print(S1[:-4])
+def d1():
+    S1 = 'root:x:bin:bash'
+    
+    print(S1[3])
+    print(S1[-1])
+    print(S1[-4:])
+    print(S1[:-4])
+
+d1()
