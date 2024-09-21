c8dd341dd286dbc0f098334b86b448c83837d193
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:35:48 +0100

    Update listRECAP3.py

--- listRECAP3.py
+++ listRECAP3.py
@@ -3,8 +3,11 @@ Author: Ms Rya
 Version: 3.10
 '''
 
+def find_colour_index(colours, colour):
+    index_number = colours.index(colour)
+    return index_number
+  
 colours = ["red", "blue", "green", "yellow"]
-index_number = colours.index("blue") 
-
+index_number = find_colour_index(colours, "blue")
 print(colours)
 print(f"The first 'blue' is at index: {index_number}")
