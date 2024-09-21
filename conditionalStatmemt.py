201b4b4dc2cf23a3525601cbc00cd5724de3f7a1
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:21:14 +0100

    Update conditionalStatmemt.py

--- conditionalStatmemt.py
+++ conditionalStatmemt.py
@@ -2,14 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
-# Given string
+def check_special_character(S, special_character):
+    if special_character in S:
+        return "Special character exists in the string."
+    else:
+        return "Special character does not exist in the string."
+
 S = '45-=itemA:50$,city1-city2'
 
-# Read a special character from <STDIN>
 special_character = input("Enter a special character: ")
 
-# Test if the input special character exists in the given string
-if special_character in S:
-    print("Special character exists in the string.")
-else:
-    print("Special character does not exist in the string.")
+print(check_special_character(S, special_character))
