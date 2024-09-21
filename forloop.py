fcd47eea4dcf01bbb7241bca977bfc1df0630e9f
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:32:10 +0100

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -2,9 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
+def count_character(s, target):
+    c = 0
+    for var in s:
+        if var == target:
+            c += 1
+    return c
+
 s = 'abcadgt6dyahpaaa'
-c=0
-for var in s:
-    if (var == 'a')
-    c = c+1
-print(c)   
+
+count_a = count_character(s, 'a')
+print(count_a)
