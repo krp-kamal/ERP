3adfa60d6153b43bd81e78d8781cb931dfec38be
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:28:57 +0100

    Update dictRECAP.py

--- dictRECAP.py
+++ dictRECAP.py
@@ -2,7 +2,9 @@
 Author: Ms Rya
 Version: 3.10
 '''
-fruits = {'apple': 5, 'banana': 3, 'cherry': 7}
+def fruit_quantities(fruits):
+    for fruit, quantity in fruits.items():
+        print(f"Fruit: {fruit}, Quantity: {quantity}")
 
-for fruit, quantity in fruits.items():
-    print(f"Fruit: {fruit}, Quantity: {quantity}")
+fruits = {'apple': 5, 'banana': 3, 'cherry': 7}
+fruit_quantities(fruits)
