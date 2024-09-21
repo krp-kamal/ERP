25895b71db6c824d7291b0914554475b1fb094e3
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:25:26 +0100

    Update dictMembershipRecap.py

--- dictMembershipRecap.py
+++ dictMembershipRecap.py
@@ -2,15 +2,16 @@
 Author: Ms Rya
 Version: 3.10
 '''
-List = {'apples': 5,'bananas': 12,'oranges': 7}
 
-# Looping through dictionary items
-for fruit, quantity in List.items():
-    print(f"We have {quantity} {fruit}.")  
+def check_inventory(stock):
+    for fruit, quantity in stock.items():
+        print(f"We have {quantity} {fruit}.")
+    
+    if 'apples' in stock:
+        print("Apples are in stock.")
 
+    if 'grapes' not in stock:
+        print("Grapes are out of stock.")
 
-if 'apples' in List:
-    print("Apples are in stock.")
-
-if 'grapes' not in List:
-    print("Grapes are out of stock.") 
+inventory = {'apples': 5, 'bananas': 12, 'oranges': 7}
+check_inventory(inventory)
