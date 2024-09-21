721e112c10ad9a60fa9f50688b65fa58d2c4292c
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 17:52:26 +0100

    Update whileLoopTask1.py

--- whileLoopTask1.py
+++ whileLoopTask1.py
@@ -2,13 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
-i = 0
-while (i<3):
-    name = input('Enter the login name')
-    if(name=='root'):
-        print('success')
-        break
-    else:
-        print('failed')
-i = i+1
-    
+def Login1():
+    i = 0
+    while (i<3):
+        name = input('Enter the login name')
+        if(name=='root'):
+            print('success')
+            break
+        else:
+            print('failed')
+            i = i+1
+Login1()    
