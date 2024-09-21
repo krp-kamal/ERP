d21197692056f9e9b8570293a68f26420fea029e
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 16:23:25 +0100

    Update tupleRECAP.py

--- tupleRECAP.py
+++ tupleRECAP.py
@@ -2,8 +2,11 @@
 Author: Ms Rya
 Version: 3.10
 '''
-my_tuple = (1, "banana", 3.14, False, [10, 20, 30])
+def display_tuple():
+    my_tuple = (1, "banana", 3.14, False, [10, 20, 30])
 
-print(my_tuple[0])  
-print(my_tuple[1])  
-print(my_tuple[4])
+    print(my_tuple[0])  
+    print(my_tuple[1])  
+    print(my_tuple[4])
+
+display_tuple()
