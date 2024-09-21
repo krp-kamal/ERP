53205e78818786478bbf949cd3378aa509ba77e0
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:43:11 +0100

    Update recap_type().py

--- recap_type().py
+++ recap_type().py
@@ -2,9 +2,13 @@
 Author: Ms Rya
 Version: 3.10
 '''
+
+def print_variable_types(x, y, z):
+    print(type(x))
+    print(type(y))
+    print(type(z))
+
 x = 10.5
 y = "Hello"
 z = [1, 2, 3]
-print(type(x)) 
-print(type(y)) 
-print(type(z))  
+print_variable_types(x, y, z)
