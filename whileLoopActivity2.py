f36cd78463fd220a4481668cfbb6fb3617eaed3f
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 16:30:49 +0100

    Update whileLoopActivity2.py

--- whileLoopActivity2.py
+++ whileLoopActivity2.py
@@ -2,13 +2,16 @@
 Author: Ms Rya
 Version: 3.10
 '''
-while True:
-    answers = input('Enter yes or no: ')
-    if answers.lower() == 'yes':
-        print('You Said yes!')
-        break
-    elif answers.lower() == 'no':
-        print('You said no!')
-        break
-    else:
-        print('Please enter either yes or no.')
+def Loops():
+    while True:
+        answers = input('Enter yes or no: ')
+        if answers.lower() == 'yes':
+            print('You Said yes!')
+            break
+        elif answers.lower() == 'no':
+            print('You said no!')
+            break
+        else:
+            print('Please enter either yes or no.')
+        
+Loops()
