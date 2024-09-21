91888b3db6c39be5f784fb5b7aea932dfed7240d
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 16:24:25 +0100

    Update tupleRECAP3.py

--- tupleRECAP3.py
+++ tupleRECAP3.py
@@ -2,7 +2,10 @@
 Author: Ms Rya
 Version: 3.10
 '''
-nested_tuple = (1, ("inner", "tuple"), 3.14, "hello", (True, False))
+def nested():
+    nested_tuple = (1, ("inner", "tuple"), 3.14, "hello", (True, False))
 
-print(nested_tuple[1][0])    
-print(nested_tuple[4][0])
+    print(nested_tuple[1][0])    
+    print(nested_tuple[4][0])
+    
+nested()
