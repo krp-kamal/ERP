1d2ecd53337a8bb0aa1b41e899e31d26cbbfdd8d
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:39:54 +0100

    Update readFileHandleRecap.py

--- readFileHandleRecap.py
+++ readFileHandleRecap.py
@@ -2,10 +2,11 @@
 Author: Ms Rya
 Version: 3.10
 '''
+def write_to_file(filename, text):
+    with open(filename, 'w') as file:
+        file.write(text)
+    print(f"Text '{text}' has been written to '{filename}'.")
+    
 filename = 'example.log'
 text_to_write = 'Hello, World!'
-
-with open(filename, 'w') as file:
-    file.write(text_to_write)
-
-print(f"Text '{text_to_write}' has been written to '{filename}'.")
+write_to_file(filename, text_to_write)
