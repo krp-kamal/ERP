47d0d09fbf36a48133b7d145626276d18c0b9c36
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:40:56 +0100

    Update recapStringMembership.py

--- recapStringMembership.py
+++ recapStringMembership.py
@@ -2,13 +2,17 @@
 Author: Ms Rya
 Version: 3.10
 '''
+def check_membership(text):
+    python_present = "Python" in text
+    java_present = "Java" in text
+    fun_not_present = "fun" not in text
+
+    return python_present, java_present, fun_not_present
+
 text = "string membership operator"
 
-python = "Python" in text       
-java = "Java" in text           
-  
-fun_present = "fun" not in text     
+python, java, fun = check_membership(text)
 
 print("Is 'Python' present in text?", python)
 print("Is 'Java' present in text?", java)
-print("Is 'fun' not present in text?", fun_present)
+print("Is 'fun' not present in text?", fun)
