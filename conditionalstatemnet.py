600818e16e4687feccb0c223584abe7ea8ce7a16
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:21:47 +0100

    Update conditionalstatemnet.py

--- conditionalstatemnet.py
+++ conditionalstatemnet.py
@@ -2,8 +2,13 @@
 Author: Ms Rya
 Version: 3.10
 '''
+
+def check_enquiry_number(enquiry_number):
+    if enquiry_number > 5000:
+        return "Enquiry number is above 5000."
+    else:
+        return "Enquiry number is not above 5000."
+
 enquiry_number = int(input("Enter business enquiry number: "))
-if enquiry_number > 5000:
-    print("Enquiry number is above 5000.")
-else:
-    print("Enquiry number is not above 5000.")
+
+print(check_enquiry_number(enquiry_number))
