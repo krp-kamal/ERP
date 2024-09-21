673090e08cc15f463cb734d2fd6e078128c9dc8e
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:40:25 +0100

    Update recapConditionalStatement.py

--- recapConditionalStatement.py
+++ recapConditionalStatement.py
@@ -2,10 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
+def check_discount_eligibility(age, is_member):
+    if age > 60 or is_member.lower() == "yes":
+        return "You are eligible for a discount."
+    else:
+        return "You are not eligible for a discount."
+
 age = int(input("Enter your age: "))
 is_member = input("Are you a member? (yes/no): ")
 
-if age > 60 or is_member == "yes":
-    print("You are eligible for a discount.")
-else:
-    print("You are not eligible for a discount.")
+result = check_discount_eligibility(age, is_member)
+print(result)
