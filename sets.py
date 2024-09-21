39bc3d04aa99e80a3affac69a7c7a63967c7392e
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 15:39:01 +0100

    Update sets.py

--- sets.py
+++ sets.py
@@ -2,15 +2,18 @@
 Author: Ms Rya
 Version: 3.10
 '''
-book_collection = set()
+def manage_Books():
+    book_collection = set()
+    
+    book_collection.add('Initialize an empty set for books')
+    book_collection.add('1984')
+    book_collection.add('Add individual books to the collection')
+    print("Book collection after adding books:", book_collection)
 
-book_collection.add('Initialize an empty set for books')
-book_collection.add('1984')
-book_collection.add('Add individual books to the collection')
-print("Book collection after adding books:", book_collection)
+    book_collection.update({'The Great Gatsby', 'Moby Dick', 'War and Peace'})
+    print("Book collection after updating with more books:", book_collection)
 
-book_collection.update({'The Great Gatsby', 'Moby Dick', 'War and Peace'})
-print("Book collection after updating with more books:", book_collection)
+    book_collection.remove('1984')
+    print("Book collection after removing a book:", book_collection)
 
-book_collection.remove('1984')
-print("Book collection after removing a book:", book_collection)
+manage_Books()
