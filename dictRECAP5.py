993c9e0df308dbb4e78301a4787f63c6160eab81
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:31:37 +0100

    Update dictRECAP5.py

--- dictRECAP5.py
+++ dictRECAP5.py
@@ -2,12 +2,14 @@
 Author: Ms Rya
 Version: 3.10
 '''
+
+def display_membership(codes):
+    for code, name in codes.items():
+        print(f'Code {code} belongs to {name}')
+
 membership_codes = {
     'AB123': 'Alice',
     'CD456': 'Bob',
     'EF789': 'Charlie'
 }
-
-for code, name in membership_codes.items():
-    print(f'Code {code} belongs to {name}')
-
+display_memberships(membership_codes)
