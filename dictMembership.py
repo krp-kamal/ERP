682b2fde22430d81cfe667a57bbde98b73df8946
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:22:48 +0100

    Update dictMembership.py

--- dictMembership.py
+++ dictMembership.py
@@ -2,6 +2,11 @@
 Author: Ms Rya
 Version: 3.10
 '''
-D = {1:'value1' , 2:'value2' , 3:'value3'}
-for var in D:
-    print('{}\t{}'.format(var,D[var]))
+
+def print_dictionary(D):
+    for var in D:
+        print('{}\t{}'.format(var, D[var]))
+
+D = {1: 'value1', 2: 'value2', 3: 'value3'}
+
+print_dictionary(D)
