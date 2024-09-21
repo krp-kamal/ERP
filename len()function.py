286d6db55c20fbe40db764d8f0998fff6a1f0001
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-21 13:32:53 +0100

    Update len()function.py

--- len()function.py
+++ len()function.py
@@ -2,16 +2,15 @@
 Author: Ms Rya
 Version: 3.10
 '''
-# Initialize two string variables
+
+def calculate_sum_of_lengths(str1, str2):
+    int1 = int(str1)
+    int2 = int(str2)
+    sum_of_lengths = len(str1) + len(str2)
+    return sum_of_lengths + int1 + int2
+
 str1 = '123'
 str2 = '4567'
 
-# Typecast the strings to integers
-int1 = int(str1)
-int2 = int(str2)
-
-# Calculate the sum of the lengths of the original strings
-sum_of_lengths = len(str1) + len(str2)
-
-# Print the results
-print("Sum of the lengths of the original strings:", sum_of_lengths + int1 + int2)
+result = calculate_sum_of_lengths(str1, str2)
+print("Sum of the lengths of the original strings:", result)
