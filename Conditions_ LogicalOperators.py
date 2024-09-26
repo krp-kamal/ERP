9300578282393cb2bfa334dfc9653da82fca1b03
krp-kamal <120426602+krp-kamal@users.noreply.github.com>
2024-09-26 15:31:19 +0100

    Update Conditions_ LogicalOperators.py

--- Conditions_ LogicalOperators.py
+++ Conditions_ LogicalOperators.py
@@ -7,11 +7,11 @@ def my_sleep():
     sleep = int(input("Enter hours of sleep last night: "))
     score = int(input("Enter your productivity score (1-10): "))
 
-    if sleep >= 8 and score >= 7:
+    if sleep >= 8 and score >= 5:
         result = "You are well rested and productive."
-    elif sleep < 8 and score >= 7:
+    elif sleep < 8 and score >= 5:
         result = "You are not well rested, but still productive."
-    elif sleep >= 8 and score < 7:
+    elif sleep >= 8 and score < 5:
         result = "You are well-rested but not very productive."
     else:
         result = "You are neither well rested nor productive."
